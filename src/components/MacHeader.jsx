ab66801a9e6d51b2114a8181d9598fa51d85b707
JBetoReyes <josereyes55@hotmail.com>
2025-02-02 17:00:39 -0600

    Add personal avatar and complete Hero component styling

---
+++ src/components/MacHeader.jsx
@@ -0,0 +1,11 @@
+export default function MacHeader() {
+    return (
+        <nav className="mac-header | margin-block-start-10">
+            <ul className="mac-header__buttons">
+                <li className="mac-header__button"></li>
+                <li className="mac-header__button"></li>
+                <li className="mac-header__button"></li>
+            </ul>
+        </nav>
+    )
+}
