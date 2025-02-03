a11a8110d0a406fbae1932409521527b7504188e
JBetoReyes <josereyes55@hotmail.com>
2025-02-02 21:42:21 -0600

    Add Expertise section with skills grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import MacHeader from './components/MacHeader'
 import About from './components/About'
+import Expertise from './components/Expertise'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <MacHeader />
         <Hero />
         <About />
+        <Expertise />
       </main>
     </>
   )
---
+++ src/components/Expertise.jsx
@@ -0,0 +1,39 @@
+import Card from './Card';
+
+export default function Expertise() {
+    const expertise = [
+        'Javasrcript',
+        'Typescript',
+        'Mong DB',
+        'SQL',
+        'HTML / CSS',
+        'Node JS',
+        'Docker',
+        'VUE',
+        'Java',
+        'Postgres',
+        'AWS Lambda',
+        'Spring Boot',
+        'React',
+        'Python',
+        'Angular',
+        'EC2',
+        'Terraform',
+        'ECS',
+        'GHA',
+        'New Relic'
+    ];
+    return (
+        <section className="expertise | margin-block-start-10">
+            <div className="container" data-type="wide">
+                <Card headingText="Top Expertise" headingClasses="clr-primary-400" >
+                    <ul className="expertise__list margin-block-start-10 margin-inline-11">
+                        {expertise.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </Card>
+            </div>
+        </section>
+    )
+}
