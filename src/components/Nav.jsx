13aa4e51b56b7128c79a04d6422e6eea3f7cf273
JBetoReyes <josereyes55@hotmail.com>
2025-02-03 17:44:17 -0600

    Close mobile menu when navigation links are clicked

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,10 +12,10 @@ export default function Nav() {
                 <img src="./Logo.svg" alt="Jose ARQ" />
             </a>
             <ul className={`nav | ${isOpen ? 'active' : ''}`}>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#expertise">Expertise</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><ToggleThemeButton /></li>
+                <li onClick={toggleMenu}><a href="#about">About Me</a></li>
+                <li onClick={toggleMenu}><a href="#expertise">Expertise</a></li>
+                <li onClick={toggleMenu}><a href="#projects">Projects</a></li>
+                <li onClick={toggleMenu}><ToggleThemeButton /></li>
             </ul>
             <div className="hamburger" onClick={toggleMenu}>
                 <span className="bar"></span>
