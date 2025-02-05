505d40685b0175004e31498981264bdfb0006292
JBetoReyes <josereyes55@hotmail.com>
2025-02-04 19:25:43 -0600

    Configure GitHub Actions build script with dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const platform = process.env.PLATFROM
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-2024/'
+  base: platform === 'github-actions' ? '/portfolio-2024/' : '/'
 })
